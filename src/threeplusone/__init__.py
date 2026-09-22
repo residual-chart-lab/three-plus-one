9cb82185a5c6ffab59fd7c1f8e117e436300a46f
residual.chart.lab <295112178+residual-chart-lab@users.noreply.github.com>
2026-09-22 09:29:28 +0000

    Export tetrahedral geometry and rotor API

--- src/threeplusone/__init__.py
+++ src/threeplusone/__init__.py
@@ -1,6 +1,24 @@
 from .core import ThreePlusOneMLP, FitResult, EpochRecord, centered_singleton_seed
 from .datasets import xnor
 from .experiments import epsilon_sweep
+from .geometry import (
+    TETRAHEDRAL_BRANCH_ANGLE,
+    centered_contrast_vertices,
+    child_axes,
+    dual_tetrahedra,
+    oriented_area,
+    tetrahedral_relative_order,
+    tetrahedron_vertices,
+)
+from .dynamics import (
+    DualRotorParams,
+    DualRotorState,
+    relative_energy,
+    relative_regime,
+    relative_winding,
+    simulate_dual_rotor,
+    total_momentum,
+)
 
 __all__ = [
     "ThreePlusOneMLP",
@@ -9,5 +27,19 @@ __all__ = [
     "centered_singleton_seed",
     "xnor",
     "epsilon_sweep",
+    "TETRAHEDRAL_BRANCH_ANGLE",
+    "centered_contrast_vertices",
+    "child_axes",
+    "dual_tetrahedra",
+    "oriented_area",
+    "tetrahedral_relative_order",
+    "tetrahedron_vertices",
+    "DualRotorParams",
+    "DualRotorState",
+    "relative_energy",
+    "relative_regime",
+    "relative_winding",
+    "simulate_dual_rotor",
+    "total_momentum",
 ]
-__version__ = "0.1.2"
+__version__ = "0.2.0"
